44ef523939bd6a940075c362db548e8bdd467b2a
mk-pmb <mk-pmb@users.noreply.github.com>
2022-03-25 01:40:34 +0100

    New folder structure for routes and handlers, debugRequestHandler

--- src/server.mjs
+++ src/server.mjs
@@ -3,7 +3,6 @@
 import absDir from 'absdir';
 import envcfgMergeConfigs from 'envcfg-merge-configs-pmb';
 import express from 'express';
-import makeRedirector from 'deviate';
 import mustBe from 'typechecks-pmb/must-be';
 import nodeHttp from 'http';
 import objPop from 'objpop';
@@ -12,9 +11,7 @@ import PrRouter from 'express-promise-router';
 import smartListen from 'net-smartlisten-pmb';
 
 import httpErrors from './httpErrors.mjs';
-import simpleFilenameRedirector from './simpleFilenameRedirector.mjs';
-import whoamiHandler from './whoamiHandler.mjs';
-
+import installRootRoutes from './routes/root.mjs';
 
 const pathInRepo = absDir(import.meta, '..');
 
@@ -27,13 +24,7 @@ const defaultConfig = {
 };
 
 
-function logIncomingRequest(req) {
-  console.debug('Incoming request:', req.method, req.url, '?', req.query);
-  req.next();
-}
-
-
-const EX = function createServer(customConfig) {
+const EX = async function createServer(customConfig) {
   const entireConfig = envcfgMergeConfigs({ ifPrefixProp: 'envcfg_prefix' },
     defaultConfig, customConfig);
   console.debug('Server config:', entireConfig);
@@ -45,7 +36,7 @@ const EX = function createServer(customConfig) {
     console.debug('cleanup:', args);
   });
 
-  const rt = PrRouter({
+  const rootRouter = PrRouter({
     strict: true,
     /* ^- Discern /session from /session/, because the trailing slash
           is relevant for relative paths.
@@ -53,35 +44,24 @@ const EX = function createServer(customConfig) {
           https://github.com/expressjs/express/issues/2281
     */
   });
-  app.use(rt);
+  app.use(rootRouter);
   app.use(httpErrors.handleUnknownError);
-
-  rt.use(logIncomingRequest);
-  rt.use('/static', express.static(popCfg('nonEmpty str', 'wwwpub_path')));
-  rt.get('/', makeRedirector('/static/'));
-
-  rt.get('/session/whoami', whoamiHandler);
-  rt.get('/session/login', makeRedirector('whoami'));
-  // ^- This route is meant to be protected by a mandatory login
-  //    requirement in a reverse proxy.
-  rt.get('/session/*', httpErrors.noSuchResource);
-
-  rt.get('/:filename', simpleFilenameRedirector('/static/:filename'));
-
   const webSrv = nodeHttp.createServer();
   webSrv.on('request', app);
 
   const srv = {
     popCfg,
     listenAddr: popCfg('str | pos0 num', 'listen_addr'),
+
     assertNoUnusedCfgOpts() {
       popCfg.expectEmpty('Unsupported server config option(s)');
     },
 
+    getRootRouter() { return rootRouter; },
     getLowLevelWebServer() { return webSrv; },
 
     async listen() {
-      const lsnSpec = smartListen(srv.listenAddr);
+      const lsnSpec = smartListen(srv.listenAddr, 0, 'http://');
       await pify(cb => webSrv.listen(lsnSpec, cb))();
       console.info('Listening on ' + lsnSpec);
     },
@@ -93,6 +73,8 @@ const EX = function createServer(customConfig) {
     },
 
   };
+
+  await installRootRoutes(srv);
 
   return srv;
 };
