dc4f93a0707ce9119a36a50af1a26c971e14426c
mk-pmb <mk-pmb@users.noreply.github.com>
2022-03-28 05:40:20 +0200

    Anno decider: Detect base ID safely.

--- src/hnd/annoDecider.mjs
+++ src/hnd/annoDecider.mjs
@@ -1,11 +1,32 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import httpErrors from '../httpErrors.mjs';
+import sendFinalTextResponse from '../finalTextResponse.mjs';
 
-import requestDebugHandler from './util/debugRequest.mjs';
+import plumb from './util/miscPlumbing.mjs';
+
+
+const baseIdRgx = /^[A-Za-z0-9_\-]{10,36}$/;
 
 
 const EX = async function makeAnnoDecider() {
-  return requestDebugHandler;
+
+  function annoHnd(req) {
+    const urlSubDirs = plumb.getFirstAsteriskDirs(req);
+    const { method } = req;
+    if (method !== 'GET') { return httpErrors.badMethod(req); }
+
+    console.debug('annoHnd:', urlSubDirs);
+    if (urlSubDirs.length !== 1) { return httpErrors.noSuchResource(req); }
+    const [baseId] = urlSubDirs;
+    const idMatch = (baseIdRgx.exec(baseId) || false);
+    if (idMatch[0] !== baseId) { return httpErrors.noSuchResource(req); }
+
+    const anno = { baseId };
+    sendFinalTextResponse.json(req, anno);
+  }
+
+  return annoHnd;
 };
 
 
