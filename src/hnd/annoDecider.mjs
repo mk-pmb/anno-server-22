3aa841c592ffdb25cf4ae834fed69fc08fe9bb2f
mk-pmb <mk-pmb@users.noreply.github.com>
2022-04-04 09:08:25 +0200

    Anno route: Better 404 errors

--- src/hnd/annoDecider.mjs
+++ src/hnd/annoDecider.mjs
@@ -12,26 +12,36 @@ const namedEqual = equal.named.deepStrictEqual;
 
 const baseIdRgx = /^[A-Za-z0-9_\-]{10,36}$/;
 
+function noSuchAnno(req, why) {
+  sendFinalTextResponse(req, {
+    code: 404,
+    text: 'Annotation not found: ' + why,
+  });
+}
+
 
 const EX = async function makeAnnoDecider(srv) {
 
-  function annoHnd(req) {
+  async function annoHnd(req) {
     const urlSubDirs = plumb.getFirstAsteriskDirs(req);
     const { method } = req;
     if (method !== 'GET') { return httpErrors.badMethod(req); }
 
-    console.debug('annoHnd:', urlSubDirs);
-    if (urlSubDirs.length !== 1) { return httpErrors.noSuchResource(req); }
+    console.debug('annoHnd: urlSubDirs =', urlSubDirs);
+    if (urlSubDirs.length !== 1) {
+      return noSuchAnno(req, 'Subresource not implemented');
+    }
     const [baseId] = urlSubDirs;
     const idMatch = (baseIdRgx.exec(baseId) || false);
-    if (idMatch[0] !== baseId) { return httpErrors.noSuchResource(req); }
+    if (idMatch[0] !== baseId) {
+      return noSuchAnno(req, 'Unsupported ID format');
+    }
 
-    console.debug('annoHnd: srv.db:', srv.db);
-    const reply = srv.db.postgresSelect('details FROM anno_data'
+    const reply = await srv.db.postgresSelect('details FROM anno_data'
       + ' WHERE base_id = $1 LIMIT 2;', [baseId]);
     const { rows } = reply;
     const nRows = rows.length;
-    if (!nRows) { return httpErrors.noSuchResource(req); }
+    if (!nRows) { return noSuchAnno(req, 'ID not in database'); }
     namedEqual('Number of rows found for anno base ID ' + baseId, nRows, 1);
     return sendFinalTextResponse.json(req, rows[0].details);
   }
