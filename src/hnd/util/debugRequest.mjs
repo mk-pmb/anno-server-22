3d0d12ce9d18d5e88f39067a5db423faa00b9e7a
mk-pmb <mk-pmb@users.noreply.github.com>
2022-04-19 02:46:35 +0200

    Log and debug handlers should be a separate module.

--- src/hnd/util/debugRequest.mjs
+++ src/hnd/util/debugRequest.mjs
@@ -1,74 +1,18 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import loMapValues from 'lodash.mapvalues';
+import makeRequestSummarizer from 'summarize-express-request-pmb';
 
 import sendFinalTextResponse from '../../finalTextResponse.mjs';
 
 
-const hnd = async function debugRequestHandler(origReq) {
-  const rq = { ...origReq };
+const summ = makeRequestSummarizer.detailed({
+  defaultBoringKeys: false,
+  lowLineBoring: false,
+});
 
-  function pop(list) {
-    const p = {};
-    list.forEach((k) => {
-      let v = rq[k];
-      let suf = '';
-      delete rq[k];
-      const t = (v && typeof v);
-      if (t === 'function') {
-        suf += '()';
-        v = v.call(origReq);
-      }
-      if (v === undefined) {
-        suf += ' ø';
-        v = origReq[k];
-      }
-      if (v === undefined) { v = 'UNDEF'; }
-      p[k + suf] = v;
-    });
-    return p;
-  }
-
-  pop([ // ignore boring keys:
-    'aborted',
-    'client',
-    'complete',
-    'end',
-    'httpVersion',
-    'httpVersionMajor',
-    'httpVersionMinor',
-    'json',
-    'next',
-    'res',
-    'send',
-    'socket',
-  ]);
-
-  const dbg = {
-    '!': pop([
-      'baseUrl',
-      'method',
-      'originalUrl',
-      'url',
-    ]),
-    ...pop([
-      'headers',
-      'params',
-      'query',
-      'rawHeaders',
-      'rawTrailers',
-      'statusCode',
-      'statusMessage',
-      'upgrade',
-    ]),
-  };
-
-  Object.keys(rq).forEach(k => (k.startsWith('_') && delete rq[k]));
-
-  // show unexpected keys:
-  const unexpected = loMapValues(rq, x => String(x && typeof x));
-  if (Object.keys(unexpected).length) { dbg.unexpected = unexpected; }
-  sendFinalTextResponse.json(origReq, dbg);
+const EX = async function debugRequestHandler(req) {
+  sendFinalTextResponse.json(req, summ(req));
 };
 
-export default hnd;
+
+export default EX;
