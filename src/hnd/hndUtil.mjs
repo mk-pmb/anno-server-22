80558bf8e5fe385a61fe502097f1cd1b031e1a54
mk-pmb <mk-pmb@users.noreply.github.com>
2022-04-20 21:16:25 +0200

    Centralize redirects, add redirect logging

--- src/hnd/hndUtil.mjs
+++ src/hnd/hndUtil.mjs
@@ -1,6 +1,23 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import deviate from 'deviate';
+
+
 const EX = {
+
+  legacyMultiArg(legacyHandlerFunc) {
+    function hnd(req) { return legacyHandlerFunc(req, req.res, req.next); }
+    return Object.assign(hnd, { legacyHandlerFunc });
+  },
+
+  makeRedirector(pattern) {
+    const redir = EX.legacyMultiArg(deviate(pattern));
+    function hnd(req) {
+      console.debug('Redirecting:', req.method, req.originalUrl, '->', pattern);
+      return redir(req);
+    }
+    return hnd;
+  },
 
   getFirstAsteriskUrlPart(req) {
     return String(req.params['0'] // Yes, express really uses a string.
--- src/hnd/sess/route.mjs
+++ src/hnd/sess/route.mjs
@@ -1,7 +1,5 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import makeRedirector from 'deviate';
-
 import httpErrors from '../../httpErrors.mjs';
 import requestDebugHandler from '../debugRequest.mjs';
 // import sendFinalTextResponse from '../../finalTextResponse.mjs';
@@ -11,14 +9,14 @@ import hndUtil from '../hndUtil.mjs';
 
 const EX = async function makeSessionRoute() {
 
-  const redirectToWhoami = makeRedirector('whoami');
+  const redirectToWhoami = hndUtil.makeRedirector('whoami');
 
   function sessionHnd(req) {
     const subUrl = hndUtil.getFirstAsteriskUrlPart(req);
     if (subUrl === 'login') {
       // This route is meant to be protected by a mandatory login
       // requirement in a reverse proxy.
-      return redirectToWhoami(req, req.res, req.next);
+      return redirectToWhoami(req);
     }
     if (subUrl === 'whoami') {
       return requestDebugHandler(req);
