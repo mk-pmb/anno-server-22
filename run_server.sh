3e9e0f4acf17af4386a74fca3f9dbded66a84454
mk-pmb <mk-pmb@users.noreply.github.com>
2022-04-20 05:33:58 +0200

    run_server.sh: Allow for better customization.

--- run_server.sh
+++ run_server.sh
@@ -2,38 +2,76 @@
 # -*- coding: utf-8, tab-width: 2 -*-
 
 
-function run_server () {
+function run_cli_main () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
   local SELFPATH="$(readlink -m -- "$BASH_SOURCE"/..)"
   cd -- "$SELFPATH" || return $?
 
-  local RUNNER='nodemjs'
+  local -A CFG=(
+    [run_task]='run_server_show_log_on_failure'
+    [lint]=
+    [run_prog]='nodemjs'
+    [log_dest]="logs.@$HOSTNAME/$FUNCNAME.log"
+    )
+  tty --silent || CFG[run_task]='actually_run_server'
+
   local ITEM=
   for ITEM in cfg.@"$HOSTNAME"{/*,.*,}.rc; do
     [ ! -f "$ITEM" ] || source -- "$ITEM" cfg:run || return $?
   done
 
-  local LOG_DEST="logs.@$HOSTNAME"
-  mkdir --parents -- "$LOG_DEST"
-  LOG_DEST+="/$FUNCNAME.log"
-  >"$LOG_DEST" || return 4$(echo "E: Cannot write to logfile: $LOG_DEST" >&2)
-  exec &> >(tee -- "$LOG_DEST")
+  "${CFG[run_task]}" "$@" || return $?
+}
 
-  if ! </dev/null "$RUNNER" -e 0; then
-    if [ -n "$npm_lifecycle_event" ]; then
-      echo "E: Running inside npm but cannot find $RUNNER." \
-        "Is the package installed correctly?" >&2
-    else
-      echo "E: $RUNNER failed. Is it in PATH? Try 'npm start' instead." >&2
-    fi
-    return 8
+
+function run_server_show_log_on_failure () {
+  ( # These parens force a subprocess with separate stdout and stderr,
+    # so their redirection to `log_dest` won't interfere with our `less`.
+    actually_run_server "$@"
+  )
+  local RV="$?"
+  [ "$RV" == 0 ] || less -S +G -- "${CFG[log_dest]}" || return $?
+  return "$RV"
+}
+
+
+function actually_run_server () {
+  tee_output_to_logfile || return $?
+  verify_run_prog || return $?
+
+  local LINT="${CFG[lint]}"
+  [ "$LINT" == + ] && LINT='elp'
+  [ -z "$LINT" ] || "$LINT" || return $?
+
+  "${CFG[run_prog]}" src/runServer.mjs "$@"
+  local RV="$?"
+  echo "D: Server quit, rv=$RV"
+  return "$RV"
+}
+
+
+function tee_output_to_logfile () {
+  mkdir --parents -- "$(dirname -- "${CFG[log_dest]}")"
+  >"${CFG[log_dest]}" || return 4$(
+    echo "E: Cannot write to logfile: ${CFG[log_dest]}" >&2)
+  exec &> >(tee -- "${CFG[log_dest]}") || return $?
+}
+
+
+function verify_run_prog () {
+  </dev/null "${CFG[run_prog]}" -e 0 && return 0
+
+  if [ -n "$npm_lifecycle_event" ]; then
+    echo "E: Running inside npm but cannot find ${CFG[run_prog]}." \
+      "Is the package installed correctly?" >&2
+  else
+    echo "E: ${CFG[run_prog]} failed. Is it in PATH? Try 'npm start' instead." >&2
   fi
-
-  exec "$RUNNER" src/runServer.mjs || return $?
+  return 8
 }
 
 
 
 
 
-run_server "$@"; exit $?
+run_cli_main "$@"; exit $?
