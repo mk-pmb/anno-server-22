b79508dfbbd2ff5aa9bf6dd5fcb03a83007d38f4
mk-pmb <mk-pmb@users.noreply.github.com>
2022-05-13 23:34:45 +0200

    debugRequest: Expose summarize function.

--- src/hnd/util/debugRequest.mjs
+++ src/hnd/util/debugRequest.mjs
@@ -5,14 +5,14 @@ import makeRequestSummarizer from 'summarize-express-request-pmb';
 import sendFinalTextResponse from '../../finalTextResponse.mjs';
 
 
-const summ = makeRequestSummarizer.detailed({
+const EX = async function debugRequestHandler(req) {
+  sendFinalTextResponse.json(req, EX.summarize(req));
+};
+
+EX.summarize = makeRequestSummarizer.detailed({
   defaultBoringKeys: false,
   lowLineBoring: false,
 });
-
-const EX = async function debugRequestHandler(req) {
-  sendFinalTextResponse.json(req, summ(req));
-};
 
 
 export default EX;
