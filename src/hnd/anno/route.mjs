76df5aacb0ddf894f7873a9a5d4682bff6051bbe
mk-pmb <mk-pmb@users.noreply.github.com>
2022-05-15 16:53:09 +0200

    Fix error message for "Anno subresource not implemented"

--- src/hnd/anno/route.mjs
+++ src/hnd/anno/route.mjs
@@ -23,10 +23,12 @@ Object.assign(EX, {
     if (req.method === 'OPTIONS') { return; }
     const urlSubDirs = hndUtil.getFirstAsteriskDirs(req);
     // console.debug('annoHnd: urlSubDirs =', urlSubDirs);
-    if (urlSubDirs.length !== 1) {
-      return httpErrors.notImpl.explain('Anno subresource not implemented'
-        + urlSubDirs[0])(req);
+    const nUrlSubDirs = urlSubDirs.length;
+    if (nUrlSubDirs >= 2) {
+      return httpErrors.notImpl.explain('Anno subresource not implemented: '
+        + urlSubDirs[1])(req);
     }
+    if (nUrlSubDirs !== 1) { throw new Error('Bad route declaration'); }
     const [annoId] = urlSubDirs;
     // req.logCkp('annoRoute', { method, annoId });
     if (annoId) { return EX.annoIdRoute(srv, req, annoId); }
