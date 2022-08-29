85fd0a87a5d413193b9735e3406ba8957d4d9397
mk-pmb <mk-pmb@users.noreply.github.com>
2022-08-29 02:00:15 +0200

    Simplify postgresSelect API for single row results.

--- src/hnd/anno/idGet.mjs
+++ src/hnd/anno/idGet.mjs
@@ -1,7 +1,5 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import equal from 'equal-pmb';
-
 import guessAndParseSubjectTargetUrl
   from 'webanno-guess-subject-target-url-pmb/extra/parse.mjs';
 
@@ -11,25 +9,28 @@ import sendFinalTextResponse from '../../finalTextResponse.mjs';
 import parseVersId from './parseVersionIdentifier.mjs';
 import ubhdAnnoIdFmt from './ubhdAnnoIdFmt.mjs';
 
-const namedEqual = equal.named.deepStrictEqual;
-
 const errNotInDb = httpErrors.noSuchAnno.explain('ID not in database');
 
 const versionSep = ubhdAnnoIdFmt.versionNumberSeparator;
 
-
-async function getExactVersion(srv, req, idParts) {
-  const { versId, baseId, versNum } = idParts;
-  const queryTpl = ('"details"'
+const queryTpl = {
+  annoDetails: ('details'
     + ' FROM anno_data WHERE base_id = $1'
     + ' AND version_num = $2 LIMIT 2'
-  );
-  const reply = await srv.db.postgresSelect(queryTpl, [baseId, versNum]);
-  const { rows } = reply;
-  const nRows = rows.length;
-  if (!nRows) { throw errNotInDb.throwable(); }
-  namedEqual('Number of rows found for anno version ID ' + versId, nRows, 1);
-  const { details } = rows[0];
+  ),
+  latestVersion: (
+    'MAX(version_num) AS latest'
+    + ' FROM anno_data WHERE base_id = $1'
+  ),
+};
+
+
+async function getExactVersion(srv, req, idParts) {
+  const { baseId, versNum } = idParts;
+
+  const { details } = (await srv.db.postgresSelect(queryTpl.annoDetails,
+    [baseId, versNum])).expectSingleRow();
+  if (details === 0) { throw errNotInDb.throwable(); }
 
   const subjTgt = guessAndParseSubjectTargetUrl(details);
   // ^-- Using parse because it includes safety checks.
@@ -44,11 +45,8 @@ async function getExactVersion(srv, req, idParts) {
 async function redirToLatestVersion(srv, req, idParts) {
   const { baseId } = idParts;
   // :ATTN:ACL: Currently no ACL checks for this lookup.
-  const queryTpl = ('MAX(version_num) AS latest'
-    + ' FROM anno_data WHERE base_id = $1'
-  );
-  const reply = await srv.db.postgresSelect(queryTpl, [baseId]);
-  const { latest } = reply.rows[0];
+  const { latest } = (await srv.db.postgresSelect(queryTpl.latestVersion,
+    [baseId])).expectSingleRow();
   if (!latest) { throw errNotInDb.throwable(); }
   return req.res.redirect(baseId + versionSep + latest);
 }
