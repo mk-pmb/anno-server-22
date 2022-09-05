a7505e95d1ab671db66c66cf976391b29d2e1c55
mk-pmb <mk-pmb@users.noreply.github.com>
2022-09-05 04:36:21 +0200

    LUsrMgr: Use the Agent model for author_identities.

--- src/cfg/lusrmgr/learnLocalUser.mjs
+++ src/cfg/lusrmgr/learnLocalUser.mjs
@@ -1,8 +1,11 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
 import makeExtendedOrderedMap from 'ordered-map-extended-pmb';
+import mustBe from 'typechecks-pmb/must-be';
+import pProps from 'p-props';
 
 import learnUpstreamUserIdAliases from './learnUpstreamUserIdAliases.mjs';
+import learnOneAuthorIdentitiy from './learnOneAuthorIdentitiy.mjs';
 
 
 function initEmptyUserRecord() {
@@ -10,23 +13,41 @@ function initEmptyUserRecord() {
     aclUserGroups: new Set(),
     authorIdentities: makeExtendedOrderedMap(),
   };
+  user.authorIdentities.byAgentId = new Map();
   return user;
 }
 
 
-const EX = function learnLocalUser(mgr, userName, mustPopDetail) {
+const EX = async function learnLocalUser(mgr, userName, mustPopDetail) {
   const user = mgr.users.getOrInit(userName, null, initEmptyUserRecord);
 
-  (mustPopDetail('undef | ary', 'acl_user_groups') || []).forEach(
+  (mustPopDetail('undef | nul | ary', 'acl_user_groups') || []).forEach(
     grpName => user.aclUserGroups.add(grpName));
 
-  user.authorIdentities.upd(mustPopDetail('undef | obj', 'author_identities'));
+  await pProps(mustPopDetail('undef | obj', 'author_identities') || false,
+    (v, k) => learnOneAuthorIdentitiy(mgr, user, k, v));
 
   learnUpstreamUserIdAliases(mgr, userName,
     mustPopDetail('undef | ary', 'upstream_userid_aliases'));
 
   mustPopDetail.done('Unsupported user account option(s)');
 };
+
+
+Object.assign(EX, {
+
+  learnMeta(ctx) {
+    const { mgr, srv, mustPopMeta } = ctx;
+    mgr.authorAgentUuidBaseUrl = (
+      mustPopMeta('str | undef', 'author_agent_uuid5_baseurl')
+      || (mustBe.tProp('Server property ', srv,
+        'nonEmpty str', 'publicBaseUrlNoSlash')
+        + '/authors/by/uuid/'));
+  },
+
+
+
+});
 
 
 export default EX;
