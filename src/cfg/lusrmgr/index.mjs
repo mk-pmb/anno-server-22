0d47c90dac43b4920cb5dad026ed31b9d01d4cde
mk-pmb <mk-pmb@users.noreply.github.com>
2022-10-09 05:07:37 +0200

    Factor out learnTopicDict.

--- src/cfg/lusrmgr/index.mjs
+++ src/cfg/lusrmgr/index.mjs
@@ -16,12 +16,11 @@ Microsoft Windows XP.
 
 
 import makeExtendedOrderedMap from 'ordered-map-extended-pmb';
-import mustBe from 'typechecks-pmb/must-be';
-import objPop from 'objpop';
-import vTry from 'vtry';
 
-import learnLocalUser from './learnLocalUser.mjs';
+import learnTopicDict from '../learnTopicDict.mjs';
+
 import learnAclUserGroup from './learnAclUserGroup.mjs';
+import learnLocalUser from './learnLocalUser.mjs';
 
 
 const EX = {
@@ -32,39 +31,14 @@ const EX = {
       upstreamUserIdAliases: new Map(),
     };
 
-    await EX.learnConfigTopic(srv, mgr, 'users', learnLocalUser);
-    await EX.learnConfigTopic(srv, mgr, 'acl_user_groups', learnAclUserGroup);
+    const ctx = { srv, mgr };
+    await learnTopicDict(ctx, 'users', learnLocalUser);
+    await learnTopicDict(ctx, 'acl_user_groups', learnAclUserGroup);
 
     // console.debug('users:', mgr.users.toDict());
     // console.debug('upstreamUserIdAliases:', mgr.upstreamUserIdAliases);
 
     return mgr;
-  },
-
-
-  async learnConfigTopic(srv, mgr, topic, how) {
-    const cfg = await srv.configFiles.readAsMap(topic);
-    const descr = 'lusrmgr: Learn config topic ' + topic;
-    const meta = { ...cfg.get('') };
-    cfg.delete('');
-    if (how.learnMeta) {
-      const mustPopMeta = objPop(meta, { mustBe }).mustBe;
-      await vTry.pr(how.learnMeta, descr + ', common settings')({
-        meta,
-        mgr,
-        mustPopMeta,
-        srv,
-      });
-      mustPopMeta.expectEmpty(descr + ': Unsupported common setting(s)');
-    }
-
-    const prs = [];
-    cfg.forEach(function learnListEntry(details, topicId) {
-      const mustPopDetail = objPop(details, { mustBe }).mustBe;
-      prs.push(vTry.pr(how, descr + ', entry ' + topicId)(
-        mgr, topicId, mustPopDetail, meta));
-    });
-    await Promise.all(prs);
   },
 
 
