497360dc76964e506cdab03be6a2ef839673a570
mk-pmb <mk-pmb@users.noreply.github.com>
2022-10-31 04:30:49 +0100

    Search by subject target URL prefix seems to work.

--- src/hnd/anno/searchBy/subjectTarget.mjs
+++ src/hnd/anno/searchBy/subjectTarget.mjs
@@ -1,6 +1,29 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-import httpErrors from '../../../httpErrors.mjs';
+import guessAndParseSubjectTargetUrl
+  from 'webanno-guess-subject-target-url-pmb/extra/parse.mjs';
+import pMap from 'p-map';
+
+// import httpErrors from '../../../httpErrors.mjs';
+import fmtAnnoCollection from '../fmtAnnosAsSinglePageCollection.mjs';
+import genericAnnoMeta from '../redundantGenericAnnoMeta.mjs';
+
+
+const searchQryTpl = `
+    "da"."base_id", "da"."version_num",
+    "da"."time_created",
+    "da"."details"
+  FROM anno_data AS da
+  JOIN (
+    SELECT base_id, MAX(version_num::smallint) AS max_revi
+    FROM anno_links
+    WHERE rel = 'subject' AND #url
+    GROUP BY base_id
+    ) AS sel
+  ON da.base_id = "sel"."base_id"
+    AND da.version_num = "sel"."max_revi"
+  ORDER BY da.time_created ASC, da.base_id ASC
+  `;
 
 
 const EX = async function bySubjectTargetPrefix(subjTgtSpec, req, srv) {
@@ -8,8 +31,32 @@ const EX = async function bySubjectTargetPrefix(subjTgtSpec, req, srv) {
     targetUrl: subjTgtSpec,
     privilegeName: 'discover',
   });
-  const msg = 'Stub! ' + subjTgtSpec;
-  throw httpErrors.notImpl.explain(msg).throwable();
+  await srv.acl.requirePerm(req, {
+    targetUrl: subjTgtSpec,
+    privilegeName: 'read',
+  });
+
+  const [urlCond, urlArg] = (subjTgtSpec.endsWith('/*')
+    ? ['starts_with(url, $1)', subjTgtSpec.slice(0, -1)]
+    : ['url = $1', subjTgtSpec]);
+  const searchQry = searchQryTpl.replace(/#url(?=\n)/, urlCond);
+  // console.debug(searchQry, { urlArg });
+  const found = await srv.db.postgresSelect(searchQry, [urlArg]);
+  // console.debug('bySubjTgt:', { found });
+  const annos = await pMap(found, async function recombineAnno(rec) {
+    const subjTgt = guessAndParseSubjectTargetUrl(rec.details);
+    await srv.acl.requirePerm(req, {
+      targetUrl: subjTgt.url,
+      privilegeName: 'read',
+    });
+    const idParts = {
+      baseId: rec.base_id,
+      versNum: rec.version_num,
+    };
+    const fullAnno = genericAnnoMeta.add(srv, idParts, rec.details);
+    return fullAnno;
+  });
+  fmtAnnoCollection.replyToRequest(srv, req, { annos });
 };
 
 
