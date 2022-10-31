faf22bec98b8e0fdd9144abfd7de3bac7bede1e1
mk-pmb <mk-pmb@users.noreply.github.com>
2022-10-31 07:19:17 +0100

    Auto-fix reverse proxy slash merging.

--- src/hnd/anno/searchBy/index.mjs
+++ src/hnd/anno/searchBy/index.mjs
@@ -9,10 +9,22 @@ const unsupportedCriterion = httpErrors.notImpl.explain(
   'Search criterion not implemented.').throwable;
 
 
+function apacheSlashes(sub) {
+  let url = sub.join('/');
+
+  // Some reverse proxies like our Apache normalize double slashes,
+  // mangling the URL. We could document how to configure them properly,
+  // … or we can just cheat-fix it:
+  url = url.replace(/^(\w+:\/)(?!\/)/, '$1/');
+
+  return url;
+}
+
+
 const EX = async function searchBy(pathParts, req, srv) {
   const [crit, ...sub] = pathParts;
   if (crit === 'subject-target') {
-    return bySubjectTarget(sub.join('/'), req, srv);
+    return bySubjectTarget(apacheSlashes(sub), req, srv);
   }
   throw unsupportedCriterion();
 };
