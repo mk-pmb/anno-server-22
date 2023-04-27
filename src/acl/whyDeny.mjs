fc78aa121a98ad8ffc0832d3d77f9c7550c87bdc
mk-pmb <mk-pmb@users.noreply.github.com>
2023-04-27 19:14:07 +0200

    Temporarily cache ACL metadata for each request.

--- src/acl/whyDeny.mjs
+++ src/acl/whyDeny.mjs
@@ -3,6 +3,7 @@
 import getOwn from 'getown';
 import mustBe from 'typechecks-pmb/must-be';
 import sortedJson from 'safe-sortedjson';
+import getOrAddKey from 'getoraddkey-simple';
 
 import httpErrors from '../httpErrors.mjs';
 
@@ -10,22 +11,26 @@ import aclSubChain from './chains/aclSubChain.mjs';
 import detectUserIdentity from './detectUserIdentity.mjs';
 
 
-const EX = async function whyDeny(req, initMeta) {
+const EX = async function whyDeny(req, actionMeta) {
   const acl = this;
 
-  const allMeta = { ...initMeta };
+  const metaCache = getOrAddKey(req, 'aclMetaCache', '{}');
+  const allMeta = { ...actionMeta };
   const mustMeta = mustBe.tProp('ACL metadata property ', allMeta);
 
-  const userIdentityMeta = await detectUserIdentity(req);
   const tgtUrl = (mustMeta('nonEmpty str | undef', 'targetUrl') || false);
-  const urlMeta = (tgtUrl
-    && req.getSrv().services.findMetadataByTargetUrl(tgtUrl));
+  const urlMeta = tgtUrl && (getOrAddKey(metaCache, 'tgtUrl:' + tgtUrl,
+    () => req.getSrv().services.findMetadataByTargetUrl(tgtUrl)));
+
+  const userMeta = await getOrAddKey(metaCache, 'user',
+    () => detectUserIdentity(req));
+  metaCache.user = userMeta;
 
   const pubMeta = {
     // ACL metadata that is ok to be "public" in the sense that it
     // may be sent to the client as part of explanation.
     // privilegeName: not here becaue it's in the main error message already.
-    userId: (userIdentityMeta.userId || ''),
+    userId: (userMeta.userId || ''),
     ...urlMeta,
   };
   Object.assign(allMeta, pubMeta);
