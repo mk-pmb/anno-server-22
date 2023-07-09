b0cade2f32adddfc997d506cdae12116d2ea1eb2
mk-pmb <mk-pmb@users.noreply.github.com>
2023-07-09 21:45:37 +0200

    Support semi-secret proprietary stamp namespaces.

--- src/hnd/anno/parseStampRows.mjs
+++ src/hnd/anno/parseStampRows.mjs
@@ -3,14 +3,27 @@
 import stampValueOrDate from './stampValueOrDate.mjs';
 
 
-const EX = function parseStampRows(rows) {
-  return Object.fromEntries(rows.map(
-    r => [(r.st_type || r.type), stampValueOrDate(r)]));
+const EX = function parseStampRows(rows, origHow) {
+  const how = (origHow || false);
+  const d = {};
+  rows.forEach(function eachRow(r) {
+    const t = (r.st_type || r.type);
+    if (t.startsWith('_')) {
+      const llf = how.lowlineFields;
+      const ok = (llf && ((llf === true)
+        || (llf.test && llf.test(t))
+      ));
+      if (!ok) { return; }
+    }
+    const v = stampValueOrDate(r);
+    d[t] = v;
+  });
+  return d;
 };
 
 
 Object.assign(EX, {
-  into(anno, stRows) { return Object.assign(anno, EX(stRows)); },
+  into(anno, stRows, how) { return Object.assign(anno, EX(stRows, how)); },
 });
 
 
