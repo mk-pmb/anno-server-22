c01556dcb3c10cd0db64a18c6e3d2a33a8a6b4af
mk-pmb <mk-pmb@users.noreply.github.com>
2023-08-16 18:27:34 +0200

    Refactor headers identity detector.

--- src/acl/idp/idfHeaders.mjs
+++ src/acl/idp/idfHeaders.mjs
@@ -1,45 +1,66 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import arrayOfTruths from 'array-of-truths';
 import loMapValues from 'lodash.mapvalues';
 
 function plainHeaderLookup(hdrName, req) { return req.header(hdrName); }
 
 
 const EX = function headers(ctx) {
-  const headerNamesLists = {};
+  const headerNamesLists = EX.learnHeaderNamesLists(ctx.popDetail);
+  // ^-- Map [session identity report field name] -> [list of header names].
 
-  function popHeaderNamesList(key, convert) {
-    const orig = ctx.popDetail('nonEmpty str | nonEmpty ary', key);
-    const list = [].concat(orig || []);
-    list.convert = convert;
-    headerNamesLists[key] = list;
-  }
-
-  popHeaderNamesList('userId', String);
-  popHeaderNamesList('renewalAvailableBefore', Number);
-  popHeaderNamesList('sessionExpiryHardLimit', Number);
-
-  const headerLookup = (ctx.headerLookupFunc || plainHeaderLookup);
-  // ^-- Easy way to plug a stub for testing and debugging.
+  const headerLookup = ctx.popDetail('fun | undef',
+    'headerLookupFunc', plainHeaderLookup);
+    // ^-- Easy way to plug a stub for testing and debugging.
 
   const det = function detectIdentityHeaders(req) {
-    const report = loMapValues(headerNamesLists, function one(list) {
-      let found = false;
-      list.some(function maybe(headerName) {
-        const raw = headerLookup(headerName, req);
-        if (!raw) { return; }
-        const val = list.convert(raw);
-        if (!val) { return; }
-        found = val;
-        return found;
-      });
-      return found;
-    });
+    const report = loMapValues(headerNamesLists,
+      hnl => EX.detectReportField(req, headerLookup, hnl));
     if (!report.userId) { return false; }
     return report;
   };
   return det;
 };
+
+
+Object.assign(EX, {
+
+  learnHeaderNamesLists(popCfg) {
+    const hnl = {};
+
+    function learnOneField(key, convert) {
+      // Lookup the list of header names that shall be used to determine
+      // the `key` field of the session identity report.
+      const list = arrayOfTruths(popCfg('nonEmpty str | nonEmpty ary', key));
+      list.convert = convert;
+      hnl[key] = list;
+    }
+
+    learnOneField('userId', String);
+    learnOneField('renewalAvailableBefore', Number);
+    learnOneField('sessionExpiryHardLimit', Number);
+    return hnl;
+  },
+
+
+  detectReportField(req, headerLookup, hnl) {
+    let found = false;
+    hnl.some(function maybe(headerName) {
+      const raw = headerLookup(headerName, req);
+      if (!raw) { return; }
+      const val = hnl.convert(raw);
+      if (!val) { return; }
+      found = val;
+      return found;
+    });
+    return found;
+  },
+
+
+});
+
+
 
 
 export default EX;
