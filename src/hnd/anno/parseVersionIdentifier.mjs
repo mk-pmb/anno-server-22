a055dee4a98596f7a736b3d3313777d3b15d7ba5
mk-pmb <mk-pmb@users.noreply.github.com>
2023-10-01 21:51:53 +0200

    Refuse requests for explicitly given version number 0.

--- src/hnd/anno/parseVersionIdentifier.mjs
+++ src/hnd/anno/parseVersionIdentifier.mjs
@@ -10,15 +10,24 @@ const replySep = ubhdAnnoIdFmt.legacyReplySeparator;
 const versIdRgx = /^([A-Za-z0-9_\-]{10,36})((?:\.[\d\.]+)*)(?:\~(\d+)|)$/;
 
 
+function parseVersNum(s, e) {
+  if (s === undefined) { return 0; }
+  const v = (+s || 0);
+  if (v >= 1) { return v; }
+  throw e('Invalid version number');
+}
+
+
 const EX = function parseVersId(errInvalidAnno, versId) {
   if (!versId) { throw errInvalidAnno('No anno ID given'); }
   const m = versIdRgx.exec(versId);
   if (!m) { throw failNoSuchAnno('Unsupported anno ID format'); }
+  const versNum = parseVersNum(m[3], errInvalidAnno);
   const parts = {
     versId,
     mongoId: m[1],
     replySuf: (m[2] || ''),
-    versNum: (+m[3] || 0),
+    versNum,
   };
   parts.baseId = parts.mongoId + parts.replySuf;
   parts.replyNums = parts.replySuf.split(replySep).slice(1).map(n => +n);
