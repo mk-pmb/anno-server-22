12aa3aaafedc00bccdfc94ddbdcc864f1dfc4e1f
mk-pmb <mk-pmb@users.noreply.github.com>
2023-11-05 20:51:33 +0100

    Stamp side effects: Set up basic infrastructure.

---
+++ src/hnd/anno/patchAnno/approvalDecisionSideEffects.mjs
@@ -0,0 +1,19 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+
+const EX = {
+
+  prepareAdd: async function sunsetUndecidedEarlierVersions() {
+    // includes: remove their "unapproved" stamps
+    console.trace('stub!');
+  },
+
+  async deleteStampUbhdUnapproved() {
+    console.trace('stub!');
+  },
+
+  cleanupAfterAdd(ctx) { return EX.deleteStampUbhdUnapproved(ctx); },
+
+};
+
+
+export default EX;
