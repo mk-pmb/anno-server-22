c3bf4c09a5a37390c7383d42faf01140d355828a
mk-pmb <mk-pmb@users.noreply.github.com>
2023-11-23 16:36:44 +0100

    Factor out the more generic part of joinStampEffUts0.

--- src/hnd/anno/miscSql.mjs
+++ src/hnd/anno/miscSql.mjs
@@ -5,6 +5,21 @@ const EX = {
   annoExactVerCond: (a, b, c) => (`${a}.base_id = ${b}.base_id `
     + `AND ${a}.version_num = ${b}.${c || 'version_num'}`),
 
+
+  joinStampEffUts0(joinAs, stType) {
+    const col = `COALESCE(${joinAs}.st_effuts, 0) AS ${joinAs}Ts`;
+    const join = `
+      LEFT JOIN anno_stamps_effuts AS ${joinAs}
+        ON ${EX.annoExactVerCond(joinAs, 'da')}
+          AND ${joinAs}.st_type = '${stType}'`;
+    return {
+      col,
+      colLn: '\n    ' + col + ',',
+      join,
+    };
+  },
+
+
 };
 
 
