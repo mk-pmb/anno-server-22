bbd750f2e9f3c79df0ae38ea6375347b2d233833
mk-pmb <mk-pmb@users.noreply.github.com>
2023-11-26 21:30:44 +0100

    joinStampEffUts0: Output all in a single line.

--- src/hnd/anno/miscSql.mjs
+++ src/hnd/anno/miscSql.mjs
@@ -8,10 +8,9 @@ const EX = {
 
   joinStampEffUts0(joinAs, stType) {
     const col = `COALESCE(${joinAs}.st_effuts, 0) AS ${joinAs}Ts`;
-    const join = `
-      LEFT JOIN anno_stamps_effuts AS ${joinAs}
-        ON ${EX.annoExactVerCond(joinAs, 'da')}
-          AND ${joinAs}.st_type = '${stType}'`;
+    const join = (`LEFT JOIN anno_stamps_effuts AS ${joinAs}`
+      + ' ON ' + EX.annoExactVerCond(joinAs, 'da')
+      + ` AND ${joinAs}.st_type = '${stType}'`);
     return {
       col,
       colLn: '\n    ' + col + ',',
