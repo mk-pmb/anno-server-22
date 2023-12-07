ec9f993cb2d6eb75b4b982e287e7dfe9d4f286cf
mk-pmb <mk-pmb@users.noreply.github.com>
2023-12-07 03:23:46 +0100

    Support explicit role name for session feature URLs.

--- src/hnd/sess/route.mjs
+++ src/hnd/sess/route.mjs
@@ -6,18 +6,21 @@ import httpErrors from '../../httpErrors.mjs';
 // import requestDebugHandler from '../util/debugRequest.mjs';
 // import sendFinalTextResponse from '../../finalTextResponse.mjs';
 
+import eternal from '../wrap/eternal.mjs';
 import plumb from '../util/miscPlumbing.mjs';
+
 import loginHandler from './login.mjs';
 import whoamiHandler from './whoami.mjs';
 
 
 const EX = async function makeSessionRoute() {
-  function sessionHnd(req) {
+  const sh = function sessionHandler(req) {
     const subUrl = plumb.getFirstAsteriskUrlPart(req);
-    const hndFunc = getOwn(EX.subHnd, subUrl);
-    return (hndFunc || httpErrors.noSuchResource)(req);
-  }
-  return sessionHnd;
+    const hndImpl = getOwn(EX.subHnd, subUrl);
+    return (hndImpl || httpErrors.noSuchResource)(req);
+  };
+  Object.assign(sh, EX.commonApi);
+  return sh;
 };
 
 
@@ -26,6 +29,14 @@ Object.assign(EX, {
   subHnd: {
     login: loginHandler,
     whoami: whoamiHandler,
+  },
+
+  commonApi: {
+
+    asRoleName: eternal(function redirectAsRoleNameSession(req, res) {
+      res.redirect('../../../session' + req.url);
+    }),
+
   },
 
 
