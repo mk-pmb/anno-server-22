40c120f314d0370234a367e18ae359acd27e00ac
mk-pmb <mk-pmb@users.noreply.github.com>
2024-04-27 01:41:20 +0200

    Factor out RSS link slot replacements for easier overview.

---
+++ src/hnd/rss/linkSlots.mjs
@@ -0,0 +1,16 @@
+// -*- coding: utf-8, tab-width: 2 -*-
+//
+// This file is meant to give an overview of available data slots in RSS
+// link templates. Implementation details belong in `fmtAnnoRssLink.mjs`.
+
+const EX = function annoRssLinkSlot(t, a, m) {
+  let u = t;
+  u = u.replace(/%as/g, a.versId);
+  u = u.replace(/%au/g, a.annoIdUrl);
+  u = u.replace(/%hu/g, a('iana:version-history'));
+  u = u.replace(/%lu/g, a('iana:latest-version'));
+  return u;
+};
+
+
+export default EX;
