b488e635d7eb98dc52a6a2d1a6c5e8e745644017
mk-pmb <mk-pmb@users.noreply.github.com>
2024-08-06 01:30:37 +0200

    Implement session login redirect.

--- src/hnd/sess/login.mjs
+++ src/hnd/sess/login.mjs
@@ -1,9 +1,37 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
+import httpErrors from '../../httpErrors.mjs';
+import validateScopeUrl from '../util/validateScopeUrl.mjs';
+// import tests from '../util/validateScopeUrl.test.mjs';
+
 import whoamiHnd from './whoami.mjs';
 
-const whoamiOpt = { detectorOpts: { occasion: 'session:login' } };
+const { fubar, genericDeny } = httpErrors.throwable;
 
-const EX = function login(req) { return whoamiHnd.hndImpl(req, whoamiOpt); };
+
+const EX = function login(req) {
+  const q = req.query;
+  if (q && q.svc && q.redir) { return EX.maybeRedir(req); }
+  return whoamiHnd.hndImpl(req, EX.whoamiOpt);
+};
+
+
+Object.assign(EX, {
+
+  whoamiOpt: { detectorOpts: { occasion: 'session:login' } },
+
+  maybeRedir(req) {
+    const srv = req.getSrv();
+    // tests.runTests(validateScopeUrl, srv);
+    const { ok, bad } = validateScopeUrl(srv, req.query.svc, req.query.redir);
+    if (bad) { throw genericDeny('Bad redirect URL: ' + bad); }
+    if (!ok) { throw fubar('Redirect URL validation was inconclusive'); }
+    req.nicerRedirect(ok);
+  },
+
+
+});
+
+
 
 export default EX;
