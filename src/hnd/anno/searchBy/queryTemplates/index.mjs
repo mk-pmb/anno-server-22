f152c30e1d87428fea545c62419b1baac074958f
mk-pmb <mk-pmb@users.noreply.github.com>
2024-08-06 09:25:39 +0200

    RSS + Search: Clarify latestOnly -> latestVersionOnly.

--- src/hnd/anno/searchBy/queryTemplates/index.mjs
+++ src/hnd/anno/searchBy/queryTemplates/index.mjs
@@ -3,7 +3,7 @@
 // for M in *.mjs; do echo "import ${M%.*} from './$M';"; done
 import contentModes from './contentModes.mjs';
 import core from './core.mjs';
-import latestOnly from './latestOnly.mjs';
+import latestVersionOnly from './latestVersionOnly.mjs';
 import orderedSearch from './orderedSearch.mjs';
 import visibility from './visibility.mjs';
 
@@ -12,7 +12,7 @@ const EX = {
   // for M in *.mjs; do echo "  ...${M%.*},"; done
   ...contentModes,
   ...core,
-  ...latestOnly,
+  ...latestVersionOnly,
   ...orderedSearch,
   ...visibility,
 };
