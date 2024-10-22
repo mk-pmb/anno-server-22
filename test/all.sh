42cb16cfe37c609293ab5dac6479826865d3e8d3
mk-pmb <mk-pmb@users.noreply.github.com>
2024-10-22 21:08:48 +0200

    [test] Run tests in the test directory rather than top level.

--- test/all.sh
+++ test/all.sh
@@ -4,12 +4,11 @@
 
 function all_tests () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
-  local REPOPATH="$(readlink -m -- "$BASH_SOURCE"/../..)"
-  cd -- "$REPOPATH" || return $?
+  local TEST_DIR="$(readlink -m -- "$BASH_SOURCE"/..)"
+  cd -- "$TEST_DIR" || return $?
 
-  echo 'D: lint: …'
-  elp || return $?
-  npm start testfx_exit_soon_sec=1
+  npm run lint || return $?
+  npm start testfx_exit_soon_sec=1 || return $?
 
   echo '+OK all tests passed.'
 }
