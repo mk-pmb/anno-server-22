40f621ff13f19c0d594bb21b181eb818cb9d85b2
mk-pmb <mk-pmb@users.noreply.github.com>
2025-05-13 15:04:02 +0200

    Refactor the contentModes CTE generation.

--- src/hnd/anno/searchBy/queryTemplates/contentModes.mjs
+++ src/hnd/anno/searchBy/queryTemplates/contentModes.mjs
@@ -1,15 +1,21 @@
 // -*- coding: utf-8, tab-width: 2 -*-
 
-function mcm(cteName, extraColumns, joins) {
-  let sql = [
-    'WITH \v_in AS (\n#|\n)',
-    'SELECT \v_in.*, ' + extraColumns + ' FROM \v_in',
-    ...joins.map(j => j + ' USING (versid)'),
-    'JOIN anno_data AS \v_data USING (versid)',
-  ].join('\n');
+function cte(cteName, sqlLines) {
+  let sql = sqlLines;
+  if (sql.join) { sql = sql.join('\n'); }
+  sql = sql.trim();
+  if (!sql.startsWith('WITH ')) { sql = 'WITH \v_in AS (\n#|\n) ' + sql; }
   sql = sql.replace(/#details/g, '\v_data.details');
   sql = sql.replace(/\v/g, cteName);
   return sql;
+}
+
+function mcm(cteName, extraColumns, joins) {
+  return cte(cteName, [
+    'SELECT \v_in.*, ' + extraColumns + ' FROM \v_in',
+    ...[].concat(joins).map(j => j + ' USING (versid)'),
+    'JOIN anno_data AS \v_data USING (versid)',
+  ]);
 }
 
 
@@ -21,10 +27,10 @@ const EX = {
     + 'NULL) AS title', []),
 
   addFullContent: mcm('full_content', 'st.stamps, #details',
-    ['LEFT JOIN anno_stamps_json AS st']),
+    'LEFT JOIN anno_stamps_json AS st'),
 
   addSubjectTargetRelUrls: mcm('subjtgt_urls', 'st.*',
-    ['LEFT JOIN anno_subjtargets_json AS st']),
+    'LEFT JOIN anno_subjtargets_json AS st'),
 
 
 };
